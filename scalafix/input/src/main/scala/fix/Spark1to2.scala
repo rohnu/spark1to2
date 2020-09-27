3d5390440dac9771c849631a99140e4fa8d095bb
Ramprasad Ohnu <41483777+rohnu@users.noreply.github.com>
2020-09-26 22:58:55 -0500

    Update Spark1to2.scala

--- scalafix/input/src/main/scala/fix/Spark1to2.scala
+++ scalafix/input/src/main/scala/fix/Spark1to2.scala
@@ -9,7 +9,7 @@ package org.apache.spark.examples
 
 
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
 
 
 object Spark1to2 {
@@ -19,18 +19,18 @@ object Spark1to2 {
       System.err.println("Usage: HdfsTest <file>")
       System.exit(1)
     }
-    val spark = SparkSession.builder.getOrCreate() // .("HdfsTest")
-    val sc = spark
-    val file = sc.read.text(args(0))
+    val spark = new SparkConf().setAppName("HdfsTest")
+    val sc = new SparkContext(spark)
+    val file = sc.textFile(args(0))
     val mapped = file.map(s => s.length).cache()
-    for (iter <- 1 to 10) {
-      val start = System.currentTimeMillis()
-      for (x <- mapped) { x + 2 }
-      val end = System.currentTimeMillis()
+        for (iter <- 1 to 10) {
+          val start = System.currentTimeMillis()
+          for (x <- mapped) { x + 2 }
+          val end = System.currentTimeMillis()
 
-      println("Iteration " + iter + " took " + (end-start) + " ms")
+    println("Iteration " + iter + " took " + (end-start) + " ms")
 
-      sc.stop()
-    }
+    sc.stop()
   }
+}
 }
